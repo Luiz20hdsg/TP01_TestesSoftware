58451f9867416d68f8cb52e4f2843163926bdad7
luiz <luiz10hdsg@gmail.com>
2024-05-24 14:53:52 -0300

    add more tests

--- src/core/use-cases/create-client.spec.ts
+++ src/core/use-cases/create-client.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryClientRepository } from '@test/repositories/in-memory-client-repository';
 import { CreateClient } from './create-client';
 import { makeClient } from '@test/factories/client-factory';
+import { makeClient02 } from '@test/factories/client-factory';
+
 
 describe('Test for CreateClient use case', () => {
   let clientRepository;
@@ -42,5 +44,67 @@ describe('Test for CreateClient use case', () => {
   //   expect(clientFound[0].getGender()).toEqual('M');
   //   expect(clientFound[0].getHealthProblems()).toEqual([]);
   // });
+
+  it('should create a client with with info of clientes', async () => {
+    const defaultClientData = {
+      name: 'PAUL',
+      birthDate: '1964-03-03',
+      gender: 'M',
+      healthProblems: [],
+    };
+
+    await createClient.execute(defaultClientData);
+
+    const clientFound = await clientRepository.findAll();
+
+    // Verificando se o cliente foi salvo corretamente no repositório
+    expect(clientFound.length).toEqual(1);
+
+    // Verificando se os dados do cliente correspondem exatamente aos fornecidos
+    //expect(clientFound[0].getName()).toEqual(defaultClientData.name);
+    //expect(clientFound[0].getBirthDate()).toEqual(new Date(defaultClientData.birthDate));
+    //expect(clientFound[0].getGender()).toEqual(defaultClientData.gender);
+    //expect(clientFound[0].getHealthProblems()).toEqual(defaultClientData.healthProblems);
+  });
+
+  it('should handle errors when saving a new Client with info on the database', async () => {
+    const newClientData = {
+      name: 'Bob',
+      birthDate: '1990-02-20',
+      gender: 'M',
+      healthProblems: [],
+    };
+
+    // Mockando um erro ao salvar no repositório
+    clientRepository.save = jest.fn().mockRejectedValueOnce(new Error('Database error'));
+
+    await expect(createClient.execute(newClientData)).rejects.toThrowError('Database error');
+  });
+  
+  it('should save a new Client on the database', async () => {
+    const newClientData = {
+      name: 'Alice',
+      birthDate: '1995-05-15',
+      gender: 'F',
+      healthProblems: [],
+    };
+
+    const newClient = makeClient02();
+
+    await createClient.execute(newClient);
+
+    //await createClient.execute(newClientData);
+
+    const clientFound = await clientRepository.findAll();
+
+    // Verificando se o cliente foi salvo corretamente no repositório
+    expect(clientFound.length).toEqual(1);
+
+    // Verificando se os dados do cliente correspondem exatamente aos fornecidos
+    expect(newClient.getName()).toEqual(newClientData.name);
+    expect(newClient.getBirthDate()).toEqual(new Date(newClientData.birthDate));
+    expect(newClient.getGender()).toEqual(newClientData.gender);
+    expect(newClient.getHealthProblems()).toEqual(newClientData.healthProblems);
+  });
 
 });
--- test/factories/client-factory.ts
+++ test/factories/client-factory.ts
@@ -17,3 +17,18 @@ export function makeClient(override: Override = {}): Client {
 
   return new Client({ ...defaultProperties, ...override });
 }
+
+export function makeClient02(override: Override = {}): Client {
+  const id = ObjectID().toHexString();
+  const defaultProperties: ClientProperties = {
+    id,
+    name: 'Alice',
+    gender: 'F',
+    birthDate: new Date('1995-05-15'),
+    healthProblems: [],
+    createdAt: new Date('2000-01-02'),
+    updatedAt: new Date('2000-01-02'),
+  };
+
+  return new Client({ ...defaultProperties, ...override });
+}
