069ce26900bc101acb9eebcd8ee54f976df08bfb
Davi Brito <daviibrito111@gmail.com>
2024-07-01 18:18:29 -0300

    test: add jest config for running integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,18 +3,24 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 import { compilerOptions } from './tsconfig.json';
 
 const config: Config.InitialOptions = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: './', // Alterado para salvar no diretório raiz do projeto
-  testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: './',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        ...pathsToModuleNameMapper(compilerOptions.paths, {
+            prefix: '<rootDir>/',
+        }),
+        '@core/(.*)': '<rootDir>/src/core/$1',
+        '@infra/(.*)': '<rootDir>/src/infra/$1',
+        '@helpers/(.*)': '<rootDir>/src/helpers/$1',
+        '@test/(.*)': '<rootDir>/test/$1',
+    },
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
 
 export default config;
